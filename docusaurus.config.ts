b6024833f1ff2c752e7770c2222a77cff9cb8259
Nathan Gross <nathan.gross@gmail.com>
2025-05-09 18:29:52 -0500

    fix: adding build/ back to .gitignore

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'govql', // Usually your GitHub org/user name.
   projectName: 'govql.github.io', // Usually your repo name.
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
